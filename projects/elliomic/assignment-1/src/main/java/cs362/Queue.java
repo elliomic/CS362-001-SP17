cc05b4d1f31473af0d3fc60554dde6fd48984f1b
Michael Elliott <elliomic@onid.orst.edu>
2017-04-17 20:55:15 -0700

    Modified path. Added code for a simple Queue.

---
+++ projects/elliomic/assignment-1/src/main/java/cs362/Queue.java
@@ -0,0 +1,55 @@
+
+public class Queue<V> {
+	private Node<V> front = null;
+	private Node<V> back = null;
+	
+	public Queue() {
+		front = null;
+		back = null;
+	}
+	
+	public boolean isEmpty() {
+		if(front == null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void enqueue(Node<V> node) {
+		if(isEmpty() == true) {
+			front = new Node<V>();
+			front.setNode(node.getNodeData());
+			front.setNext(back);
+			front.setLast(back);
+			back = front;
+		}
+		else {
+			Node<V> oldBack = back;
+			back = new Node<V>();
+			back.setNode(node.getNodeData());
+			back.setNext(null);
+			back.setLast(oldBack);
+			oldBack.setNext(back);
+		}
+	}
+	
+	public Node<V> dequeue() {
+		if(isEmpty() == true) {
+			return null;
+		}
+		else {
+			Node<V> returnNode = back;
+			back = back.getPrev();
+			return returnNode;
+		}
+	}
+	
+	public Node<V> getFront() {
+		if(isEmpty() == true) {
+			return null;
+		}
+		return front;
+	}
+}
