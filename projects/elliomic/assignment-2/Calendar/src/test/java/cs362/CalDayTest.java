75894e17652f4eab4d536b8ec3e021877c069b72
Michael Elliott <elliomic@onid.orst.edu>
2017-05-01 18:06:46 -0700

    Added CalDay tests

--- projects/elliomic/assignment-2/Calendar/src/test/java/cs362/CalDayTest.java
+++ projects/elliomic/assignment-2/Calendar/src/test/java/cs362/CalDayTest.java
@@ -5,6 +5,7 @@ package cs362;
  */
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Iterator;
 
 
 import org.junit.Test;
@@ -12,10 +13,86 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalDayTest {
+	public static Appt validAppt = new Appt(13,30,10,4,2017, "Birthday Party",
+	                                        "This is my birthday party.");
+	public static Appt validAppt2 = new Appt(12,30,10,4,2017, "Birthday Party",
+	                                         "This is my birthday party.");
 
-	 @Test
-	  public void test01()  throws Throwable  {
-
-	 }
 	
+	/**
+	 * Tests that the CalDay constructors work as expected.
+	 */
+	@Test
+	public void test01()  throws Throwable  {
+		CalDay cal = new CalDay();
+		assertFalse(cal.isValid());
+
+		cal = new CalDay(new GregorianCalendar());
+		assertTrue(cal.isValid());
+	}
+
+	/**
+	 * Tests that the addAppt method works as expected.
+	 * Tests that the getSizeAppts method works as expected.
+	 */
+	@Test
+	public void test02()  throws Throwable  {
+		CalDay cal = new CalDay(new GregorianCalendar());
+
+		cal.addAppt(validAppt);
+		cal.addAppt(validAppt2);
+
+		assertEquals(2, cal.getSizeAppts());
+		assertEquals(2, cal.getAppts().size());
+	}
+
+	/**
+	 * Tests that the isValid method works as expected.
+	 */
+	@Test
+	public void test03()  throws Throwable  {
+		CalDay cal = new CalDay();
+		assertEquals(cal.valid, cal.isValid());
+	}
+
+	/**
+	 * Tests that the CalDay iterator constructor works as expected.
+	 */
+	@Test
+	public void test04()  throws Throwable  {
+		CalDay cal = new CalDay();
+		Iterator<?> iter = cal.iterator();
+		assertNull(iter);
+
+		cal = new CalDay(new GregorianCalendar());
+		iter = cal.iterator();
+		assertNotNull(iter);
+	}
+
+	/**
+	 * Tests that the CalDay gets methods work as expected.
+	 */
+	@Test
+	public void test05()  throws Throwable  {
+		CalDay cal = new CalDay(new GregorianCalendar(2017, 4, 13));
+
+		assertEquals(13, cal.getDay());
+		assertEquals(4, cal.getMonth());
+		assertEquals(2017, cal.getYear());
+	}
+
+	/**
+	 * Tests that the toString method works as expected.
+	 */
+	@Test
+	public void test06()  throws Throwable  {
+		CalDay cal = new CalDay(new GregorianCalendar(2017, 4, 13));
+
+		cal.addAppt(validAppt);
+		cal.addAppt(validAppt2);
+
+		assertEquals("\t --- 4/13/2017 --- \n --- -------- Appointments ------------ --- \n"
+		             + validAppt2.toString() + " " + validAppt.toString() + " \n",
+		             cal.toString());
+	}
 }
